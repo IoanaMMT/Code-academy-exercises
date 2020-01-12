63d4cd7957f64115370e57627d0fda97f2712593
ioanaMMT <ioana2morosanu@gmail.com>
2020-01-12 17:52:45 +0000

    Dice game

--- codeacademy.py
+++ codeacademy.py
@@ -11,6 +11,8 @@ if len(word_game) > 0 and word_game.isalpha():
 print new_word'''
 
 # This program is meant to be an Area Calculator
+
+'''
 print "Helo, the calculator is ready"
 
 option = raw_input("What shape would you like to calculate? enter C for Circle or T for Triangle:")
@@ -30,3 +32,38 @@ else:
  print "Invalid shape."
 
 print "The program is exiting"  
+'''
+
+# Guess the number
+from random import randint
+from time import sleep
+def get_user_guess():
+  guess = int(raw_input("Guess a number:"))
+  return guess
+
+def roll_dice(number_of_sides):
+  first_roll = randint(1, number_of_sides)
+  second_roll = randint(1, number_of_sides)
+  max_val = number_of_sides * 2
+  print "The maximum passible value is: %d "%  max_val
+  guess = get_user_guess()
+  if guess > max_val:
+    print("Sorry, but the number you guess must be   smaler than the maximum value")
+  else:
+    print("Rolling...")
+    sleep(2)
+    print("First roll is %d "% first_roll)
+    sleep(1)
+    print("Second roll is %d "% second_roll)
+    sleep(1)
+    total_roll = first_roll + second_roll
+    print("The total is %d "% total_roll)
+    sleep(1)
+    print("Results...")
+    sleep(1)
+    if guess == total_roll:
+      print("You won!")
+    else:
+      print("You lost!")
+    
+roll_dice(6)
